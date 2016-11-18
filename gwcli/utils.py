b74cebda3e08383b10d18314442dae3eb41295da
Paul Cuzner <pcuzner@redhat.com>
2016-11-18 14:36:10 +1300

    gwcli.utils: added API class to handle the http requests
    The APIRequest class handles interaction with the api and
    catches connection errors if the endpoint is down

--- gwcli/utils.py
+++ gwcli/utils.py
@@ -4,6 +4,9 @@ __author__ = 'pcuzner@redhat.com'
 # import ceph_iscsi_config.settings as settings
 import socket
 import json
+import requests
+
+import ceph_iscsi_config.settings as settings
 
 
 def readcontents(filename):
@@ -56,29 +59,41 @@ class GatewayError(Exception):
 
 
 class GatewayAPIError(GatewayError):
-
-    def __init__(self, msg):
-        msg_js = json.loads(msg)
-        self.msg = msg_js['message']
-    def __str__(self):
-        return repr(self.msg)
+    pass
 
 
 class GatewayLIOError(GatewayError):
     pass
 
+class APIRequest(object):
 
-# def console_message(message, colour='green'):
-#
-#     colour_map = {'blue': '\033[94m',
-#                   'green': '\033[92m',
-#                   'yellow': '\033[93m',
-#                   'red': '\033[91m'}
-#
-#     end_sequence = '\033[0m'
-#
-#     if colour in colour_map:
-#         print colour_map[colour] + message + end_sequence
-#     else:
-#         print message
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
 
+        # Establish defaults for the API connection
+        if 'auth' not in self.kwargs:
+            self.kwargs['auth'] = (settings.config.api_user, settings.config.api_password)
+        if 'verify' not in self.kwargs:
+            self.kwargs['verify'] = settings.config.api_ssl_verify
+
+        self.http_methods = ['get', 'put',  'delete']
+        self.data = None
+
+    def _get_response(self):
+        return self.data
+
+    def __getattr__(self, name):
+        if name in self.http_methods:
+            request_method = getattr(requests, name)
+            try:
+                self.data = request_method(*self.args, **self.kwargs)
+            except requests.ConnectionError:
+                raise GatewayAPIError("Unable to connect to api endpoint @ {}".format(self.args[0]))
+            else:
+                # since the attribute is a callable, we must return with a callable
+                return self._get_response
+        raise AttributeError()
+
+    response = property(_get_response,
+                        doc="get http response output")
