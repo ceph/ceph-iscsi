2873e7dffe88a6586edac0f589a1ef28efb90a52
Paul Cuzner <pcuzner@redhat.com>
2017-01-11 14:46:27 +1300

    setup: version bump to 2.1

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class StripExtension(distutils.command.install_scripts.install_scripts):
 
 setup(
     name="gwcli",
-    version="2.0",
+    version="2.1",
     description="cli interface to iscsi gateways based on configshell_fb modules",
     long_description=long_description,
     author="Paul Cuzner",
