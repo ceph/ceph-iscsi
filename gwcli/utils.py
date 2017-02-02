d693a3d22d282aa7b062717dd8b0334fa3571dcb
Paul Cuzner <pcuzner@redhat.com>
2017-02-02 17:36:11 +1300

    utils: updated console_message to change output if running in batch mode
    console message now uses the settings.config.interactive attr to determine
    whether color control chars should be added to the output. When running in
    batch mode these chars make the output difficult to parse.

--- gwcli/utils.py
+++ gwcli/utils.py
@@ -14,8 +14,8 @@ class Colors(object):
 
     map = {'green': '\x1b[32;1m',
            'red': '\x1b[31;1m',
-           ' yellow': '\x1b[33;1m',
-           ' blue': '\x1b[34;1m'}
+           'yellow': '\x1b[33;1m',
+           'blue': '\x1b[34;1m'}
 
 def readcontents(filename):
     with open(filename, 'r') as input_file:
@@ -120,9 +120,15 @@ def progress_message(text, color='green'):
     sys.stdout.flush()
 
 def console_message(text, color='green'):
-    print("{}{}{}".format(Colors.map[color],
-                          text,
-                          '\x1b[0m'))
+
+    color_needed = getattr(settings.config, 'interactive', True)
+
+    if color_needed:
+        print("{}{}{}".format(Colors.map[color],
+                              text,
+                              '\x1b[0m'))
+    else:
+        print(text)
 
 def get_port_state(ip_address, port):
     """
