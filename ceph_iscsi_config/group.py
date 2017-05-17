1fba958c1506898b5f26ce97934893d2c0a8df0d
Paul Cuzner <pcuzner@redhat.com>
2017-05-17 14:10:10 +1200

    group: updated to handle updates to the group definition
    groups may now be added, changed and removed from the config
    object

--- ceph_iscsi_config/group.py
+++ ceph_iscsi_config/group.py
@@ -4,15 +4,26 @@
 
 from ceph_iscsi_config.common import Config
 from ceph_iscsi_config.client import GWClient
-# from ceph_iscsi_config.utils import convert_2_bytes
+from ceph_iscsi_config.utils import ListComparison
 
 __author__ = 'pcuzner@redhat.com'
 
 
 class Group(object):
 
-    def __init__(self, logger, group_name, members=[], disks=[],
-                 mode='create'):
+    def __init__(self, logger, group_name, members=[], disks=[]):
+
+        """
+        Manage a host group definition. The input for the group object is the
+        desired state of the group where the logic enforced produces an
+        idempotent group definition across API/CLI and more importantly Ansible
+
+        :param logger: (logging object) used for centralised logging
+        :param group_name: (str) group name
+        :param members: (list) iscsi IQN's of the clients
+        :param disks: (list) disk names of the format pool.image
+        """
+
         self.logger = logger
 
         self.error = False
@@ -31,14 +42,13 @@ class Group(object):
         self.group_name = group_name
         self.group_members = members
         self.disks = disks
-        self.mode = mode
 
     @staticmethod
     def _check_config(logger, config_object):
 
         if 'groups' in config_object.config:
-            logger.debug("Config object contains a 'groups' section - upgrade "
-                         "not required")
+            logger.debug("Config object contains a 'groups' section - config "
+                         "object upgrade is not required")
             return
         else:
             # Need to upgrade the config object to include the new
@@ -61,13 +71,6 @@ class Group(object):
         self.error_msg = error_msg
         self.logger.debug("Error: {}".format(self.error_msg))
 
-    def manage(self):
-
-        if self.mode == 'create':
-            self.apply()
-        else:
-            self.purge()
-
     def _valid_client(self, client_iqn, config):
 
         client = config['clients'].get(client_iqn, {})
@@ -86,6 +89,8 @@ class Group(object):
     def apply(self):
 
         if self.group_name not in self.config.config['groups']:
+
+            # New Group definition
             self.logger.debug("Group mgmt processing new request for "
                               "{}".format(self.group_name))
             if len(set(self.group_members)) != len(self.group_members):
@@ -124,21 +129,9 @@ class Group(object):
             image_list = self.disks
             for mbr in self.group_members:
 
-                client = GWClient(self.logger, mbr, image_list, '')
-                client.define_client()      # set up clients ACL
-
-                # grab the metadata from the current definition
-                client.metadata = self.config.config['clients'][mbr]
-                client.setup_luns()
-
-                if client.error:
-                    self._set_error(client.error_msg)
+                self.update_client(mbr, image_list)
+                if self.error:
                     return
-                else:
-                    self.logger.info("Updating config object for "
-                                     "client '{}'".format(mbr))
-                    client.metadata['group_name'] = self.group_name
-                    self.config.update_item("clients", mbr, client.metadata)
 
             # update the config object to include the new group definition
             self.logger.debug("Adding group definition to the config object")
@@ -148,9 +141,83 @@ class Group(object):
             self.config.commit()
 
         else:
-            # this is an existing definition
+
+            # Existing Group definition update
             self.logger.debug("Updating existing group: "
                               "{}".format(self.group_name))
+
+            this_group = self.config.config['groups'][self.group_name]
+            members = ListComparison(this_group.get('members'),
+                                     self.group_members)
+            disks = ListComparison(this_group.get('disks'),
+                                   self.disks)
+
+            group_changes = (members.added | members.removed | disks.added |
+                             disks.removed)
+
+            if not group_changes:
+                # no changes required
+                self.logger.info("Current group definition matches request "
+                                 "- no changes needed")
+                return
+
+            # At this point we know there are changes to make
+            all_disks = self.config.config['disks'].keys()
+            all_clients = self.config.config['clients'].keys()
+            if not members.added.issubset(set(all_clients)) or \
+               not disks.added.issubset(set(all_disks)):
+                self._set_error("Invalid disk(s)/member(s) requested - disk/"
+                                "client iqn must exist in the configuration")
+                return
+
+            # at this point the disk list and member list are valid
+            image_list = self.disks
+            if len(disks.added | disks.removed) > 0:
+                # process all clients in the group with the new
+                # image list
+                for mbr in self.group_members:
+
+                    self.update_client(mbr, image_list)
+                    if self.error:
+                        return
+
+            if members.removed:
+                for mbr in members.removed:
+                    self.remove_client(mbr)
+                # remove the member(s) from the group
+
+            this_group['members'] = self.group_members
+            this_group['disks'] = self.disks
+
+            self.config.update_item("groups", self.group_name, this_group)
+            self.config.commit()
+
+    def update_client(self, client_iqn, image_list):
+
+        client = GWClient(self.logger, client_iqn, image_list, '')
+        client.define_client()                          # set up clients ACL
+
+        # grab the metadata from the current definition
+        client.metadata = self.config.config['clients'][client_iqn]
+        client.setup_luns()
+
+        if client.error:
+            self._set_error(client.error_msg)
+            return
+        else:
+            self.logger.info("Updating config object for "
+                             "client '{}'".format(client_iqn))
+            client.metadata['group_name'] = self.group_name
+            self.config.update_item("clients", client_iqn, client.metadata)
+
+    def remove_client(self, client_iqn):
+        client_md = self.config.config["clients"][client_iqn]
+
+        # remove the group_name setting from each client
+        client_md['group_name'] = ''
+        self.config.update_item("clients", client_iqn, client_md)
+        self.logger.info("Removed {} from group {}".format(client_iqn,
+                                                           self.group_name))
 
     def purge(self):
 
@@ -159,15 +226,13 @@ class Group(object):
         groups = self.config.config['groups']
         if self.group_name in groups:
             for mbr in groups[self.group_name]["members"]:
-                client_md = self.config.config["clients"][mbr]
-
-                # remove the group_name setting from each client
-                client_md['group_name'] = ''
-                self.config.update_item("clients", mbr, client_md)
+                self.remove_client(mbr)
 
             # issue a del_item to the config object for this group_name
             self.config.del_item("groups", self.group_name)
             self.config.commit()
+            self.logger.info("Group {} removed".format(self.group_name))
         else:
+
             self._set_error("Group name requested does not exist")
             return
