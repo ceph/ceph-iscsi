f089033c724f716fdf5b9f885a398ae7e0a3c3e3
Mike Christie <mchristi@redhat.com>
2017-12-22 22:25:32 -0600

    fix gateway_keyring setting
    The default gateway_keyring is set as the filename and path,
    but when the user sets it it can only be the filename because
    we merge it with "/etc/ceph".
    
    This fixes the initialization so it is only the filename
    and it also has us use the ceph_config_dir instead of
    hard coding /etc/ceph.

--- ceph_iscsi_config/common.py
+++ ceph_iscsi_config/common.py
@@ -32,7 +32,9 @@ class CephCluster(object):
         self.error = False
         self.error_msg = ''
         self.cluster = rados.Rados(conffile=settings.config.cephconf,
-                                   conf=dict(keyring="/etc/ceph/{}".format(settings.config.gateway_keyring)))
+                                   conf=dict(keyring="{}/{}".format(
+                                             settings.config.ceph_config_dir,
+                                             settings.config.gateway_keyring)))
         try:
             self.cluster.connect()
         except rados.Error as err:
