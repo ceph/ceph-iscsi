648f25a796f1ca9775c6189f167bade7c2d335d6
Mike Christie <mchristi@redhat.com>
2020-05-23 16:32:44 -0500

    api: Add call to get device status/state
    This patch has rbd-target-api track the tcmu-runner service status to
    figure out if a disk is failing over too much, experiencing cmd
    timeouts, etc. It can then report this info with the disk GET call like
    
    curl --insecure --user admin:admin -X GET
    http://ans6:5000/api/disk/rbd/test1
    {
      "allocating_host": "ans8",
      "backstore": "user:rbd",
      "backstore_object_name": "rbd.test1",
      "controls": {},
      "created": "2020/03/22 18:59:08",
      "image": "test1",
      "owner": "ans6",
      "pool": "rbd",
      "pool_id": 1,
      "status": {
        "gateways": {
          "ans6": {
            "cmd_timed_out_cnt": 0,
            "conn_lost_cnt": 0,
            "lock_lost_cnt": 11
          },
          "ans8": {
            "cmd_timed_out_cnt": 0,
            "conn_lost_cnt": 0,
            "lock_lost_cnt": 54
          }
        },
        "lock_owner": "ans6",
        "state": "Online"
      },
      "updated": "2020/03/22 18:59:35",
      "wwn": "aa6c4b17-ec78-400b-9818-a1089aa7dd68"
    }
    
    Signed-off-by: Mike Christie <mchristi@redhat.com>

---
+++ ceph_iscsi_config/device_status.py
@@ -0,0 +1,155 @@
+import json
+import rados
+import threading
+import time
+
+import ceph_iscsi_config.settings as settings
+
+
+class StatusCounter(object):
+    def __init__(self, name, cnt):
+        self.name = name
+        self.cnt = cnt
+        self.last_cnt = cnt
+
+
+class TcmuDevStatusTracker(object):
+    def __init__(self, image_name):
+        self.image_name = image_name
+        self.gw_counter_lookup = {}
+        self.lock_owner = ""
+        self.state = "Online"
+        self.changed_state = False
+        self.stable_cnt = 0
+
+    def get_status_dict(self):
+        status = {}
+
+        status['state'] = self.state
+        status['lock_owner'] = self.lock_owner
+        status['gateways'] = {}
+
+        for gw, stat_cnt_dict in self.gw_counter_lookup.items():
+            status['gateways'][gw] = {}
+
+            for name, stat_cnt in stat_cnt_dict.items():
+                status['gateways'][gw][name] = stat_cnt.cnt
+
+        return status
+
+    def check_for_degraded_state(self, stat_cnt):
+        if stat_cnt.name == "cmd_timed_out_cnt" or \
+           stat_cnt.name == "conn_lost_cnt":
+            if abs(stat_cnt.cnt - stat_cnt.last_cnt) >= 1:
+                self.state = "Degraded - cluster access failure"
+                self.changed_state = True
+                self.stable_cnt = 0
+                return
+
+        if stat_cnt.name == "lock_lost_cnt" and \
+           abs(stat_cnt.cnt - stat_cnt.last_cnt) >= \
+           settings.config.lock_lost_cnt_thresh:
+            self.state = "Degraded - excessive failovers"
+            self.changed_state = True
+            self.stable_cnt = 0
+            return
+
+    def update_status(self, gw, status):
+        if status is None:
+            # Sometimes status calls will return empty statuses even though
+            # there is valid data. We might not see it until the Nth call.
+            return
+
+        counter_dict = self.gw_counter_lookup.get(gw)
+        if counter_dict is None:
+            counter_dict = {}
+
+        for name, val in status.items():
+            if name == "lock_owner" and val == "true":
+                self.lock_owner = gw
+                continue
+
+            if name != "cmd_timed_out_cnt" and name != "conn_lost_cnt" and \
+               name != "lock_lost_cnt":
+                continue
+
+            stat_cnt = counter_dict.get(name)
+            if stat_cnt is None:
+                stat_cnt = StatusCounter(name, int(val))
+
+            stat_cnt.cnt = int(val)
+            # TODO:
+            # If we detect a degraded state, we can throttle the path here.
+            self.check_for_degraded_state(stat_cnt)
+            stat_cnt.last_cnt = stat_cnt.cnt
+
+            counter_dict[name] = stat_cnt
+
+        self.gw_counter_lookup[gw] = counter_dict
+
+
+class DeviceStatusWatcher(threading.Thread):
+    def __init__(self, logger):
+        threading.Thread.__init__(self)
+        self.logger = logger
+        self.daemon = True
+        self.cluster = None
+        self.status_lookup = {}
+
+    def get_dev_status(self, image_name):
+        return self.status_lookup.get(image_name)
+
+    def exit(self):
+        if self.cluster:
+            self.cluster.shutdown()
+
+    def run(self):
+        self.cluster = rados.Rados(conffile=settings.config.cephconf,
+                                   name=settings.config.cluster_client_name)
+        self.cluster.connect()
+
+        while True:
+            time.sleep(settings.config.status_check_interval)
+
+            cmd = json.dumps({"prefix": "service status", "format": "json"})
+
+            ret, outb, outs = self.cluster.mon_command(cmd, b'')
+            if ret != 0:
+                self.logger.error("mon command failed {}".format(ret))
+                continue
+
+            svc = json.loads(outb).get('tcmu-runner')
+            if svc is None:
+                continue
+
+            image_names_dict = {}
+            for daemon, daemon_info in svc.items():
+                gw, image_name = daemon.split(":", 1)
+                image_names_dict[image_name] = image_name
+
+                dev_status = self.get_dev_status(image_name)
+                if dev_status is None:
+                    dev_status = TcmuDevStatusTracker(image_name)
+                    self.status_lookup[image_name] = dev_status
+
+                dev_status.update_status(gw, daemon_info.get('status'))
+
+            # cleanup stale entries and try to move to online if a dev
+            # didn't not see any errors on every gateway for a while
+            for image_name, dev_status in self.status_lookup.items():
+                if image_names_dict.get(image_name) is None:
+                    del self.status_lookup[image_name]
+                else:
+                    if dev_status.changed_state is False:
+                        dev_status.stable_cnt += 1
+
+                        if dev_status.stable_cnt > settings.config.stable_state_reset_cnt:
+                            dev_status.stable_cnt = 0
+                            dev_status.state = "Online"
+                    else:
+                        dev_status.changed_state = False
+
+            # debugging info
+            dev_status = self.get_dev_status(image_name)
+            stats_dict = dev_status.get_status_dict()
+            self.logger.error(stats_dict)
