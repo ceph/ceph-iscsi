ceph/ceph-iscsi

commit d0f023a6a19ab2ce619826bac4bf93ffb42dc272

api: Add call to get device status/state
This patch has rbd-target-api track the tcmu-runner service status to
figure out if a disk is failing over too much, experiencing cmd
timeouts, etc. It can then report this info with the disk GET call like

curl --insecure --user admin:admin -X GET
http://ans6:5000/api/disk/rbd/test1
{
  "allocating_host": "ans8",
  "backstore": "user:rbd",
  "backstore_object_name": "rbd.test1",
  "controls": {},
  "created": "2020/03/22 18:59:08",
  "image": "test1",
  "owner": "ans6",
  "pool": "rbd",
  "pool_id": 1,
  "status": {
    "gateways": {
      "ans6": {
        "cmd_timed_out_cnt": 0,
        "conn_lost_cnt": 0,
        "lock_lost_cnt": 11
      },
      "ans8": {
        "cmd_timed_out_cnt": 0,
        "conn_lost_cnt": 0,
        "lock_lost_cnt": 54
      }
    },
    "lock_owner": "ans6",
    "state": "Online"
  },
  "updated": "2020/03/22 18:59:35",
  "wwn": "aa6c4b17-ec78-400b-9818-a1089aa7dd68"
}

Signed-off-by: Mike Christie <mchristi@redhat.com>
Signed-off-by: Xiubo Li <xiubli@redhat.com>

ceph_iscsi_config/device_status.py
@@ -1,0 +1,162 @@
+import json
+import rados
+import threading
+import time
+from datetime import datetime
+
+import ceph_iscsi_config.settings as settings
+
+
+class StatusCounter(object):
+    def __init__(self, name, cnt):
+        self.name = name
+        self.cnt = cnt
+        self.last_cnt = cnt
+
+
+class TcmuDevStatusTracker(object):
+    def __init__(self, image_name):
+        self.image_name = image_name
+        self.gw_counter_lookup = {}
+        self.lock_owner = ""
+        self.lock_owner_timestamp = None
+        self.state = "Online"
+        self.changed_state = False
+        self.stable_cnt = 0
+
+    def get_status_dict(self):
+        status = {}
+
+        status['state'] = self.state
+        status['lock_owner'] = self.lock_owner
+        status['gateways'] = {}
+
+        for gw, stat_cnt_dict in self.gw_counter_lookup.items():
+            status['gateways'][gw] = {}
+
+            for name, stat_cnt in stat_cnt_dict.items():
+                status['gateways'][gw][name] = stat_cnt.cnt
+
+        return status
+
+    def check_for_degraded_state(self, stat_cnt):
+        if stat_cnt.name in ["cmd_timed_out_cnt", "conn_lost_cnt"]:
+            if abs(stat_cnt.cnt - stat_cnt.last_cnt) >= 1:
+                self.state = "Degraded - cluster access failure"
+                self.changed_state = True
+                self.stable_cnt = 0
+                return
+
+        if stat_cnt.name == "lock_lost_cnt" and \
+           abs(stat_cnt.cnt - stat_cnt.last_cnt) >= \
+           settings.config.lock_lost_cnt_threshhold:
+            self.state = "Degraded - excessive failovers"
+            self.changed_state = True
+            self.stable_cnt = 0
+            return
+
+    def update_status(self, gw, status, status_stamp):
+        if status is None:
+            # Sometimes status calls will return empty statuses even though
+            # there is valid data. We might not see it until the Nth call.
+            return
+
+        counter_dict = self.gw_counter_lookup.get(gw)
+        if counter_dict is None:
+            counter_dict = {}
+
+        for name, val in status.items():
+            if name == "lock_owner" and val == "true":
+                dt = datetime.strptime(status_stamp, "%Y-%m-%dT%H:%M:%S.%f%z")
+                if self.lock_owner_timestamp is None or dt > self.lock_owner_timestamp:
+                    self.lock_owner_timestamp = dt
+                    self.lock_owner = gw
+                    self.stable_cnt = 0
+                continue
+
+            if name not in ["cmd_timed_out_cnt", "conn_lost_cnt", "lock_lost_cnt"]:
+                continue
+
+            stat_cnt = counter_dict.get(name)
+            if stat_cnt is None:
+                stat_cnt = StatusCounter(name, int(val))
+
+            stat_cnt.cnt = int(val)
+            # TODO:
+            # If we detect a degraded state, we can throttle the path here.
+            self.check_for_degraded_state(stat_cnt)
+            stat_cnt.last_cnt = stat_cnt.cnt
+
+            counter_dict[name] = stat_cnt
+
+        self.gw_counter_lookup[gw] = counter_dict
+
+
+class DeviceStatusWatcher(threading.Thread):
+    def __init__(self, logger):
+        threading.Thread.__init__(self)
+        self.logger = logger
+        self.daemon = True
+        self.cluster = None
+        self.status_lookup = {}
+
+    def get_dev_status(self, image_name):
+        return self.status_lookup.get(image_name)
+
+    def exit(self):
+        if self.cluster:
+            self.cluster.shutdown()
+
+    def run(self):
+        self.cluster = rados.Rados(conffile=settings.config.cephconf,
+                                   name=settings.config.cluster_client_name)
+        self.cluster.connect()
+
+        while True:
+            time.sleep(settings.config.status_check_interval)
+
+            cmd = json.dumps({"prefix": "service status", "format": "json"})
+
+            ret, outb, outs = self.cluster.mgr_command(cmd, b'')
+            if ret != 0:
+                self.logger.error("mgr command failed {}".format(ret))
+                continue
+
+            svc = json.loads(outb).get('tcmu-runner')
+            if svc is None:
+                self.logger.warning("there is no tcmu-runner data avaliable")
+                self.state = "Unknown"
+                continue
+
+            image_names_dict = {}
+            for daemon, daemon_info in svc.items():
+                gw, image_name = daemon.split(":", 1)
+                image_names_dict[image_name] = image_name
+
+                dev_status = self.get_dev_status(image_name)
+                if dev_status is None:
+                    dev_status = TcmuDevStatusTracker(image_name)
+                    self.status_lookup[image_name] = dev_status
+
+                dev_status.update_status(gw, daemon_info.get('status'),
+                                         daemon_info.get('status_stamp'))
+
+            # cleanup stale entries and try to move to online if a dev
+            # didn't not see any errors on every gateway for a while
+            for image_name, dev_status in self.status_lookup.items():
+                if image_names_dict.get(image_name) is None:
+                    del self.status_lookup[image_name]
+                else:
+                    if dev_status.changed_state is False:
+                        dev_status.stable_cnt += 1
+
+                        if dev_status.stable_cnt > settings.config.stable_state_reset_count:
+                            dev_status.stable_cnt = 0
+                            dev_status.state = "Online"
+                    else:
+                        dev_status.changed_state = False
+
+            # debugging info
+            dev_status = self.get_dev_status(image_name)
+            stats_dict = dev_status.get_status_dict()
+            self.logger.debug(stats_dict)

ceph_iscsi_config/gateway_setting.py
@@ -204,3 +204,8 @@ TCMU_SETTINGS = {
     "qfull_timeout": IntSetting("qfull_timeout", 0, 600, 5),
     "osd_op_timeout": IntSetting("osd_op_timeout", 0, 600, 30),
     "hw_max_sectors": IntSetting("hw_max_sectors", 1, 8192, 1024)}
+
+TCMU_DEV_STATUS_SETTINGS = {
+    "lock_lost_cnt_threshhold": IntSetting("lock_lost_cnt_threshhold", 1, 1000000, 12),
+    "status_check_interval": IntSetting("status_check_interval", 1, 600, 10),
+    "stable_state_reset_count": IntSetting("stable_state_reset_count", 1, 600, 3)}

ceph_iscsi_config/settings.py
@@ -11,7 +11,8 @@ import json
 import re
 
 from ceph_iscsi_config.gateway_setting import (TGT_SETTINGS, SYS_SETTINGS,
-                                               TCMU_SETTINGS)
+                                               TCMU_SETTINGS,
+                                               TCMU_DEV_STATUS_SETTINGS)
 
 
 # this module when imported preserves the global values
@@ -63,12 +64,17 @@ class Settings(object):
         self._add_attrs_from_defs(SYS_SETTINGS)
         self._add_attrs_from_defs(TGT_SETTINGS)
         self._add_attrs_from_defs(TCMU_SETTINGS)
+        self._add_attrs_from_defs(TCMU_DEV_STATUS_SETTINGS)
 
         if len(dataset) != 0:
             # If we have a file use it to override the defaults
             if config.has_section("config"):
                 self._override_attrs_from_conf(config.items("config"),
                                                SYS_SETTINGS)
+
+            if config.has_section("device_status"):
+                self._override_attrs_from_conf(config.items("device_status"),
+                                               TCMU_DEV_STATUS_SETTINGS)
 
             if config.has_section("target"):
                 all_settings = TGT_SETTINGS.copy()
@@ -129,6 +135,7 @@ class Settings(object):
         self._hash_settings(SYS_SETTINGS.keys(), sync_settings)
         self._hash_settings(TGT_SETTINGS.keys(), sync_settings)
         self._hash_settings(TCMU_SETTINGS.keys(), sync_settings)
+        self._hash_settings(TCMU_DEV_STATUS_SETTINGS.keys(), sync_settings)
 
         h = hashlib.sha256()
         h.update(json.dumps(sync_settings).encode('utf-8'))

rbd-target-api.py
@@ -36,6 +36,7 @@ from ceph_iscsi_config.common import Config
 from ceph_iscsi_config.utils import (normalize_ip_literal, resolve_ip_addresses,
                                      ip_addresses, read_os_release, encryption_available,
                                      CephiSCSIError, this_host)
+from ceph_iscsi_config.device_status import DeviceStatusWatcher
 
 from gwcli.utils import (APIRequest, valid_gateway, valid_client,
                          valid_credentials, get_remote_gateways, valid_snapshot_name,
@@ -1008,7 +1009,15 @@ def disk(pool, image):
     if request.method == 'GET':
 
         if image_id in config.config['disks']:
-            return jsonify(config.config["disks"][image_id]), 200
+            disk_dict = config.config["disks"][image_id]
+            global dev_status_watcher
+            disk_status = dev_status_watcher.get_dev_status(image_id)
+            if disk_status:
+                disk_dict['status'] = disk_status.get_status_dict()
+            else:
+                disk_dict['status'] = {'state': 'Unknown'}
+
+            return jsonify(disk_dict), 200
 
         else:
             return jsonify(message="rbd image {} not "
@@ -2893,6 +2902,10 @@ def main():
     if settings.config.log_to_file:
         log.addHandler(file_handler)
     log.addHandler(syslog_handler)
+
+    global dev_status_watcher
+    dev_status_watcher = DeviceStatusWatcher(logger)
+    dev_status_watcher.start()
 
     ceph_gw = CephiSCSIGateway(logger, config)
 

commit 518b810a88e092eb8009704b1a4a62ac55ce436e

device_status: fix a typo
d0f023a6a19ab2ce619826bac4bf93ffb42dc272 introduced a small typo in a
warning message when there's no tcmu-runner data available.
This commit fixes it.

Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>

ceph_iscsi_config/device_status.py
@@ -124,7 +124,7 @@ class DeviceStatusWatcher(threading.Thread):
 
             svc = json.loads(outb).get('tcmu-runner')
             if svc is None:
-                self.logger.warning("there is no tcmu-runner data avaliable")
+                self.logger.warning("there is no tcmu-runner data available")
                 self.state = "Unknown"
                 continue
 